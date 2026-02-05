3a169aeb2958580fc555717bc2efbcea71d10aff
frodnie838 <frodnie838@g.educaand.es>
2026-02-05 20:05:17 +0100

    inicio, esencial Auth

--- src/app/modules/auth/auth.ts
+++ src/app/modules/auth/auth.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { ActorLogin } from '../../models/actor-login';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
-  templateUrl: './auth.html',
-  styleUrl: './auth.css',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './auth.html'
 })
-export class Auth {
+export class AuthComponent {
+  user: ActorLogin = { username: '', password: '' };
 
+  constructor(private authService: AuthService) {}
+
+  onLogin() {
+    this.authService.login(this.user).subscribe({
+      next: (res) => alert('Login exitoso: ' + res.username),
+      error: (err) => alert('Error de acceso')
+    });
+  }
 }
