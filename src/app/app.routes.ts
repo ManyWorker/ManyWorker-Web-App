f0f3dd924f11424b269158c257957dd4033adcda
frodnie838 <frodnie838@g.educaand.es>
2026-02-06 13:11:31 +0100

    2.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './modules/auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: AuthComponent},
+];
